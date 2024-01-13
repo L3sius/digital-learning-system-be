a2d6788bddb2bd607a61608a00dab6e834fe53c8
Justas <justaslescinskas@gmail.com>
2024-01-13 17:38:05 +0200

    Update

--- src/main/java/justas/lescinskas/digitallearningsystembe/configuration/ApplicationSecurityConfig.java
+++ src/main/java/justas/lescinskas/digitallearningsystembe/configuration/ApplicationSecurityConfig.java
@@ -1,8 +1,36 @@
 package justas.lescinskas.digitallearningsystembe.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
-public class ApplicationConfig {
-    
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((request) -> request.requestMatchers("/helloworld").permitAll());
+        httpSecurity.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource())).csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "UPDATE", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
 }
--- src/main/java/justas/lescinskas/digitallearningsystembe/controller/HelloController.java
+++ src/main/java/justas/lescinskas/digitallearningsystembe/controller/HelloController.java
@@ -1,2 +1,12 @@
-package justas.lescinskas.digitallearningsystembe.controller;public class HelloController {
+package justas.lescinskas.digitallearningsystembe.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/helloworld")
+    public String hello() {
+        return "Hello World";
+    }
 }
